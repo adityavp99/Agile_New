5ef17a3a1419e5369c2e0622deb328f12db299a3
Aditya Pandhari <adityavp99@gmail.com>
2022-08-12 22:37:48 +0800

    Added NavBar without Styling

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import HomePage from "./pages/Home";
 import RestaurantsPage from "./pages/Restaurants";
 import HelpPage from "./pages/Help";
 
+import Navbar from "./components/layout/Navbar";
+
 function App() {
   return (
+    <div>
+      <Navbar />
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/restaurants" element={<RestaurantsPage />} />
       <Route path="/help" element={<HelpPage />} />
     </Routes>
+    </div>
   );
 }
 
