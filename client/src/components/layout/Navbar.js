afa919b8cd50c2da74392fa86e72c0581181b025
Aditya Pandhari <adityavp99@gmail.com>
2022-08-20 00:23:14 +0800

    added navbar template with styling

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,19 +1,36 @@
 import { Link } from "react-router-dom";
-// import navStyle from  "./Navbar.module.css"
+
+import navStyle from "./Navbar.module.css";
+
+import profileIcon from "./profile.jpg";
+import companylogo from "./takeoutlogo.jpg";
+import cartIcon from "./cart.png";
 
 function Navbar() {
   return (
-    <nav className="nav">
-     <Link to="/" className="logo">Site Logo</Link> 
+    <nav className={navStyle.nav}>
+      <div className={navStyle.logosholder}>
+        <img src={profileIcon} id={navStyle.logo1} alt="Profile Icon" />
+        <Link to="/" className={navStyle.link}>
+          <img src={companylogo} id={navStyle.logo2} alt="TakeOut Logo" />
+        </Link>
+        <img src={cartIcon} id={navStyle.logo3} alt="Cart Icon" />
+      </div>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/" className={navStyle.link}>
+            Home
+          </Link>
         </li>
         <li>
-          <Link to="/restaurants">Restaurants</Link>
+          <Link to="/restaurants" className={navStyle.link}>
+            Restaurants
+          </Link>
         </li>
         <li>
-          <Link to="/help">Help</Link>
+          <Link to="/help" className={navStyle.link}>
+            Help
+          </Link>
         </li>
       </ul>
     </nav>
