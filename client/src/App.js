9923b223ef1fd8cbe6b3e2d9f582c9160e4d1bdc
nachiapp004 <nachiappankannappan21@gmail.com>
2022-08-23 15:32:42 +0800

    Finished categories page with styling

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Home";
 import RestaurantsPage from "./pages/Restaurants";
 import HelpPage from "./pages/Help";
+import CategoriesPage from "./pages/categories/Categories";
 
 import Navbar from "./components/layout/Navbar";
 
@@ -14,6 +15,7 @@ function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/restaurants" element={<RestaurantsPage />} />
       <Route path="/help" element={<HelpPage />} />
+      <Route path="/categories" element={<CategoriesPage />} />
     </Routes>
     </div>
   );
---
+++ client/src/pages/categories/Categories.js
@@ -0,0 +1,29 @@
+import categoriesCSS from "./Categories.module.css";
+
+import halalImg from "./images/halal.jpg";
+import indianImg from "./images/indian.jpg";
+import vegetarianImg from "./images/vegetarian.jpg";
+import japaneseImg from "./images/japanese.jpg"; 
+
+
+function CategoriesPage(){
+return <div className={categoriesCSS.categories}>
+            <h3> Trending Now </h3>
+            <div className={categoriesCSS.subholder}>
+                
+                <p className={categoriesCSS.label1}>Halal</p>
+                <img src={halalImg} className={categoriesCSS.image1} alt="Explore" />
+                
+                <p className={categoriesCSS.label2}>Indian</p>
+                <img src={indianImg} className={categoriesCSS.image2} alt="Categories" />
+
+                <p className={categoriesCSS.label3}>Vegetarian</p>
+                <img src={vegetarianImg} className={categoriesCSS.image3} alt="Bubble Tea" />
+                
+                <p className={categoriesCSS.label4}>Japanese</p>
+                <img src={japaneseImg} className={categoriesCSS.image4} alt="Pasta" />
+            </div>    
+        </div>
+}
+
+export default CategoriesPage;
