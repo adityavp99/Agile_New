26b54fcc4a46150a910ce11958333e636b2ba377
Charli <mushhall@gmail.com>
2022-08-28 04:18:26 +0800

    cart working

--- client/src/pages/cart/Cart.js
+++ client/src/pages/cart/Cart.js
@@ -1,21 +1,23 @@
+import { useState } from "react";
+
 import cartCSS from "./Cart.module.css";
 
-import pizzaImg from "../../images/burger.jpg";
+import burger from "../../images/burger.jpg";
 
 function CartPage() {
     return (
         <div className={cartCSS.Green}>
             <div className={cartCSS.Yellow}>
 
-                <div>
+                <div className={cartCSS.ordertop}>
                     <h2 className={cartCSS.headertext}>Restaurant Name</h2>
                     <div className={cartCSS.Order}>
-                        <img src={pizzaImg} className={cartCSS.orderImg} alt="pizza"/>
+                        <img src={burger} className={cartCSS.orderImg} alt="burger"/>
                         <h4 className={cartCSS.ordertext}>Item Name: </h4>
-
+                        <MyForm />
                     </div>
                     <div className={cartCSS.picture}>
-
+                        <img src={burger} className={cartCSS.pictureImg} alt="burger"/>
                     </div>
                 </div>
 
@@ -32,6 +34,35 @@ function CartPage() {
             </div>
         </div>
     );
+}
+
+function MyForm() {
+    const [inputs, setInputs] = useState({});
+  
+    const handleChange = (event) => {
+      const name = event.target.name;
+      const value = event.target.value;
+      setInputs(values => ({...values, [name]: value}))
+    }
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      console.log(inputs);
+    }
+
+    return(
+        <form onSubmit={handleSubmit} className={cartCSS.form}>
+            <label>Amount:
+            <input 
+                type="number" 
+                name="age" 
+                value={inputs.age || ""} 
+                onChange={handleChange}
+                />
+            </label>
+            <input type="submit" />
+        </form>
+    )
 }
 
 export default CartPage;
